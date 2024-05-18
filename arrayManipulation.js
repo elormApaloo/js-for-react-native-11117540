0b435053b22c26d5703e15d9aed49ad670f3e5d0
elormApaloo <apalooelorm7@gmail.com>
2024-05-18 16:03:29 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -18,6 +18,29 @@ const processedArray = processArray(inputArray)
 
 console.log(processedArray);
 
+// Task 2
+function formatArrayStrings(strings, numbers) {
+return strings.map((str, index)=> {
+if (numbers[index] % 2 === 0){
+return str.toUpperCase();
+}else{
+    return str.toLowerCase();
+}
+});
+}
+
+// Example Usage
+const inputNumbers = [2, 4, 6, 8, 10];
+const processedNumbers = processArray(inputNumbers);
+const inputStrings = [ "Jane", "is", "good", "at", "basketball"];
+const formattedStrings = formatArrayStrings(inputStrings, processedNumbers);
+console.log(formattedStrings);
+
+module.exports = {
+processArray,
+formatArrayStrings
+};
+
 
 
 
